ab4f8ea6558bdff869c3e3d267b1c12b22d495e7
Master <aydincinar01@hotmail.com>
2022-03-21 11:16:16 +0000

    db connection complated and set up relationship between back and frond hand

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//all employees route
 router.get('/', (req, res) => {
     // res.send('Hello world!');
     res.render('index');
--- server.js
+++ server.js
@@ -8,16 +8,22 @@ connectDB();
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const { engine } = require('express/lib/application');
+const bodyParser = require('body-parser');
+
+//const { engine } = require('express/lib/application');
+
 const indexRouter = require('./routes/index');
+const employeeRouter = require('./routes/employees');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
+app.use('/employees', employeeRouter);
 
 mongoose.connection.on('error', error => {
     console.error('Dn Connection error : ' + error);
